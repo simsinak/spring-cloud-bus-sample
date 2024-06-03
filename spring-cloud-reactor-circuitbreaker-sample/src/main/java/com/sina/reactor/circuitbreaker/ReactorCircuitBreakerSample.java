d9c3e6c6cbdad61218b66aeba82c24ca3b08684d
sinaaskarnejad <sina.askarnejad@gmail.com>
2024-06-03 18:34:33 +0330

    [enhancement]add reactor circuit breaker sample

---
+++ spring-cloud-reactor-circuitbreaker-sample/src/main/java/com/sina/reactor/circuitbreaker/ReactorCircuitBreakerSample.java
@@ -0,0 +1,13 @@
+package com.sina.reactor.circuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorCircuitBreakerSample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorCircuitBreakerSample.class, args);
+    }
+
+}
